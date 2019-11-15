09aa1e12be9fa66f474c13d2872e51a42407e8cb
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-15 20:54:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -75,7 +75,7 @@ class MusicLibraryController
     song_number = gets
     sorted_songs = Song.all.sort_by{|song| song.name}
     if song_number.to_i >= 1 && song_number.to_i <= sorted_songs.count
-      binding.pry
+      #binding.pry
       $stdout.puts("Playing #{sorted_songs[song_number.to_i-1].name} by #{sorted_songs[song_number.to_i -1].artist.name}")
     end
     #binding.pry
